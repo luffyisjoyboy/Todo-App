6b062254dc234774d04e2935d740b891484cf406
Akhilesh naredla <anaredla@Luffy.local>
2024-12-10 00:04:55 -0600

    added user todos logic

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,10 +1,41 @@
+import { title } from "node:process"
+import { todo } from "node:test"
+
 const usersExpress = require('express')
 const usersRouter = usersExpress.Router()
+const usersAuthmiddleware = require('../middleware')
+const usersPrisma = require("../db_connection")
 
-usersRouter.get("/home", (req:any, resp:any)=>{
-    resp.json({
-        msg:"Test 1234"
+usersRouter.get("/todos", usersAuthmiddleware, async (req:any, resp:any)=>{
+    await usersPrisma.$connect()
+    try{
+        let user_todos = await usersPrisma.user.findMany({
+            where: {
+                id: req.id
+            },
+            select: {
+                id: true,
+                Todos: {
+                    select: {
+                        title:true,
+                        description: true,
+                        status: true
+                    }
+                }
+            },
+        })
+        return resp.json({
+            user_todos
+        })
+    }catch(err){
+        resp.json({
+        msg:"No Todos to fetch",
+        err
     })
+    } finally{
+        await usersPrisma.$disconnect()
+    }
+
 })
 
 module.exports = usersRouter
